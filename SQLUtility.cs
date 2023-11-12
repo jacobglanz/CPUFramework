d7a483ab81895d33f333bea66fafe5a44480679b
jacobglanz <jacobglanz9@gmail.com>
2023-11-12 13:20:12 -0500

    initial commit

---
+++ SQLUtility.cs
@@ -0,0 +1,26 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace CPUFramework
+{
+    public class SQLUtility
+    {
+        public static string ConnectionString = "";
+
+        public static DataTable GetDateTable(string sqlstatment)
+        {
+            Debug.Print(sqlstatment);
+            DataTable dt = new();
+            SqlConnection conn = new();
+            conn.ConnectionString = ConnectionString;
+            conn.Open();
+            var cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = sqlstatment;
+            var dr = cmd.ExecuteReader();
+            dt.Load(dr);
+            return dt;
+        }
+    }
+}
