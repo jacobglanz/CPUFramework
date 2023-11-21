418a7ca07c9c397b876ceb28035c03a08db1c0ae
jacobglanz <jacobglanz9@gmail.com>
2023-11-21 13:25:28 -0500

    Added: DBconnection, sproc, GetDateTable with cmd

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -8,23 +8,38 @@ namespace CPUFramework
     {
         public static string ConnectionString = "";
 
+        public static SqlCommand GetSQLCommand(string sproc)
+        {
+            SqlCommand cmd = new();
+
+            using (SqlConnection conn = new(ConnectionString))
+            {
+                cmd = new() { Connection = conn, CommandType = CommandType.StoredProcedure, CommandText = sproc };
+                conn.Open();
+                SqlCommandBuilder.DeriveParameters(cmd);
+            }
+            
+            return cmd;
+        }
+
+        public static DataTable GetDateTable(SqlCommand cmd)
+        {
+            Debug.Print($"{Environment.NewLine}{cmd.CommandText}--------");
+            DataTable dt = new();
+            using (SqlConnection conn = new(ConnectionString))
+            {
+                cmd.Connection = conn;
+                conn.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                dt.Load(dr);
+            }
+            SetAllColumnsAllowNull(dt);
+            return dt;
+        }
+
         public static DataTable GetDateTable(string sqlstatment)
         {
-            Debug.Print(sqlstatment);
-            DataTable dt = new();
-            SqlConnection conn = new();
-            conn.ConnectionString = ConnectionString;
-            conn.Open();
-
-            var cmd = new SqlCommand();
-            cmd.Connection = conn;
-            cmd.CommandText = sqlstatment;
-            var dr = cmd.ExecuteReader();
-            dt.Load(dr);
-
-            SetAllColumnsAllowNull(dt);
-
-            return dt;
+            return GetDateTable(new SqlCommand(sqlstatment));
         }
 
         public static void ExecuteSQL(string sql)
@@ -36,19 +51,19 @@ namespace CPUFramework
         {
             int n = 0;
             DataTable dt = GetDateTable(sql);
-            if(dt.Columns.Count > 0 && dt.Rows.Count > 0)
+            if (dt.Columns.Count > 0 && dt.Rows.Count > 0)
             {
-                if(dt.Rows[0][0] != DBNull.Value)
+                if (dt.Rows[0][0] != DBNull.Value)
                 {
                     int.TryParse(dt.Rows[0][0].ToString(), out n);
-                }                
+                }
             }
             return n;
         }
 
         private static void SetAllColumnsAllowNull(DataTable dt)
         {
-            foreach(DataColumn c in dt.Columns)
+            foreach (DataColumn c in dt.Columns)
             {
                 c.AllowDBNull = true;
             }
@@ -56,9 +71,9 @@ namespace CPUFramework
 
         public static void DebugPrintDataTable(DataTable dt)
         {
-            foreach(DataRow r in dt.Rows)
+            foreach (DataRow r in dt.Rows)
             {
-                foreach(DataColumn c in dt.Columns)
+                foreach (DataColumn c in dt.Columns)
                 {
                     Debug.Print(c.ColumnName + " = " + r[c.ColumnName]);
                 }
